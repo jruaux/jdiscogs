5416e15d1d00347f47b4cc7e474fa8711891c9cb
julien <julien@localhost>
2020-02-09 22:56:03 -0800

    catching number format exception

--- src/main/java/org/ruaux/jdiscogs/data/xml/Track.java
+++ src/main/java/org/ruaux/jdiscogs/data/xml/Track.java
@@ -18,7 +18,8 @@ import lombok.extern.slf4j.Slf4j;
 public @Data class Track {
 
 	private final static Pattern cdTrackPattern = Pattern.compile("^((?<disc>\\d+)[\\.\\-])?(?<track>\\d+)");
-	private final static Pattern vinylTrackPattern = Pattern.compile("^(?<side>[a-zΑ-Ω])(?<track>\\d+)?", Pattern.CASE_INSENSITIVE);
+	private final static Pattern vinylTrackPattern = Pattern.compile("^(?<side>[a-zΑ-Ω\\wа-яА-Я])(?<track>\\d+)?",
+			Pattern.CASE_INSENSITIVE);
 	private final static Pattern durationPattern = Pattern.compile("^(?<minutes>\\d+)\\:(?<seconds>\\d+)");
 
 	@XmlElement(name = "position")
@@ -40,34 +41,47 @@ public @Data class Track {
 	}
 
 	public TrackNumber getTrackNumber() {
-		if (position==null || position.isBlank())  {
-			return null;
+		if (position != null && !position.isBlank()) {
+			Matcher cdMatcher = cdTrackPattern.matcher(position.trim());
+			if (cdMatcher.find()) {
+				String disc = cdMatcher.group("disc");
+				try {
+					int track = Integer.parseInt(cdMatcher.group("track"));
+					Integer discNumber = disc == null ? null : Integer.parseInt(disc);
+					return TrackNumber.builder().discNumber(discNumber).trackNumber(track).build();
+				} catch (NumberFormatException e) {
+					log.error("Could not parse cd track position {}", position);
+				}
+			} else {
+				Matcher vinylMatcher = vinylTrackPattern.matcher(position.trim());
+				if (vinylMatcher.find()) {
+					String track = vinylMatcher.group("track");
+					try {
+						Integer trackNumber = track == null ? null : Integer.parseInt(track);
+						return TrackNumber.builder().side(vinylMatcher.group("side")).trackNumber(trackNumber).build();
+					} catch (NumberFormatException e) {
+						log.error("Could not parse vinyl track position {}", position);
+					}
+				} else {
+					log.error("Could not match track position to CD or Vinyl formats '{}'", position);
+				}
+			}
 		}
-		Matcher cdMatcher = cdTrackPattern.matcher(position.trim());
-		if (cdMatcher.find()) {
-			String disc = cdMatcher.group("disc");
-			int track = Integer.parseInt(cdMatcher.group("track"));
-			return TrackNumber.builder().discNumber(disc == null ? null : Integer.parseInt(disc)).trackNumber(track)
-					.build();
-		}
-		Matcher vinylMatcher = vinylTrackPattern.matcher(position);
-		if (vinylMatcher.find()) {
-			String track = vinylMatcher.group("track");
-			return TrackNumber.builder().side(vinylMatcher.group("side"))
-					.trackNumber(track == null ? null : Integer.parseInt(track)).build();
-		}
-		log.error("Could not parse track number '{}'", position);
 		return null;
 	}
 
 	public Integer getDurationInSeconds() {
 		Matcher matcher = durationPattern.matcher(duration.trim());
 		if (matcher.matches()) {
-			return Integer.parseInt(matcher.group("minutes")) * 60 + Integer.parseInt(matcher.group("seconds"));
+			try {
+				return Integer.parseInt(matcher.group("minutes")) * 60 + Integer.parseInt(matcher.group("seconds"));
+			} catch (NumberFormatException e) {
+				log.error("Could not parse duration ''", duration);
+			}
 		}
 		return null;
 	}
-	
+
 	public static void main(String[] args) {
 		Track track = new Track();
 		track.setPosition("Α3");
